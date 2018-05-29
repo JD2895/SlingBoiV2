1dbc7d371da3e587075e6ad3a9528260ef9f1aac
JD2895 <j.davis.2895@gmail.com>
2018-05-29 00:11:02 -0400

    Added Player Seeking Enemy
    Added Player Seeking Enemy

--- Assets/Scripts/SeekPlayer.cs
+++ Assets/Scripts/SeekPlayer.cs
@@ -6,18 +6,30 @@ public class SeekPlayer : MonoBehaviour {
 
     public GameObject player_obj;
     public float movespeed;
+    public float turn_rate;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //transform.LookAt(player_obj.transform);
-        //transform.position += transform.forward * movespeed * Time.deltaTime;
+        Vector3 forward_dir = -transform.right;
+        Vector3 player_dir = this.transform.position - player_obj.transform.position;
+        float angle = Vector3.SignedAngle(forward_dir, player_dir, transform.forward);
+        
+        if (angle > 30.0)
+        {
+            transform.Rotate(Vector3.forward, turn_rate);
+        } else if (angle < -30.00)
+        {
+            transform.Rotate(Vector3.forward, -turn_rate);
+        }
+        
+        transform.position = transform.position + ((transform.right).normalized * movespeed);
 
-        Vector3 diff = transform.position - player_obj.transform.position;
-        transform.position = transform.position - diff.normalized * movespeed;
+
+
     }
 }
